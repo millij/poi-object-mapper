ae76c0dee6ccc42ac9b4be00d94eb785bc7fecde
mayankanand7 <mayank@admavin.com>
2024-12-17 17:39:08 +0530

    [Test]: added test cases for the method

--- src/test/java/io/github/millij/poi/ss/writer/XlsxWriterTest.java
+++ src/test/java/io/github/millij/poi/ss/writer/XlsxWriterTest.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
@@ -110,6 +112,65 @@ public class XlsxWriterTest {
         // Add Sheets
         gew.addSheet(Employee.class, employees);
         gew.addSheet(Company.class, companies);
+
+        // Write
+        gew.write(filepath_output_file);
+    }
+
+    @Test
+    public void test_write_map_as_xlsx_sheet() throws IOException {
+        final String filepath_output_file = _path_test_output.concat("map_2_sheets.xlsx");
+
+        // Excel Writer
+        LOGGER.info("test_write_map_as_xlsx_sheet :: Writing to file - {}", filepath_output_file);
+        SpreadsheetWriter gew = new XlsWriter();
+
+        // Headers
+        final List<String> headers = new ArrayList<>();
+        headers.add("Slno.");
+        headers.add("Name");
+        headers.add("Age");
+        headers.add("Gender");
+        headers.add("Height (mts)");
+        headers.add("Address");
+
+        // Data
+        final List<Map<String, String>> rowsDataMap = new ArrayList<>();
+        final Map<String, String> rowDataMap = new HashMap<>();
+        rowDataMap.put("Slno.", "1");
+        rowDataMap.put("Name", "foo");
+        rowDataMap.put("Age", "1");
+        rowDataMap.put("Gender", "Male");
+        rowDataMap.put("Height (mts)", "1.6");
+        rowDataMap.put("Address", "Chennai, India");
+        rowsDataMap.add(rowDataMap);
+
+        // Add Sheets
+        gew.addSheet(rowsDataMap, "test_sheet", headers);
+
+        // Write
+        gew.write(filepath_output_file);
+    }
+
+    @Test
+    public void test_write_map_as_template_xlsx_sheet() throws IOException {
+        final String filepath_output_file = _path_test_output.concat("map_2_template_sheets.xlsx");
+
+        // Excel Writer
+        LOGGER.info("test_write_map_as_template_xlsx_sheet :: Writing to file - {}", filepath_output_file);
+        SpreadsheetWriter gew = new XlsWriter();
+
+        // Headers
+        final List<String> headers = new ArrayList<>();
+        headers.add("Slno.");
+        headers.add("Name");
+        headers.add("Age");
+        headers.add("Gender");
+        headers.add("Height (mts)");
+        headers.add("Address");
+
+        // Add Sheets
+        gew.addSheet(new ArrayList<>(), "test_sheet", headers);
 
         // Write
         gew.write(filepath_output_file);
